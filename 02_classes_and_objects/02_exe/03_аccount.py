56019311164165dffc5ed2fbffd002d9446d6e0c
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-07-20 12:22:38 +0300

    Create 03_аccount.py

---
+++ 02_classes_and_objects/02_exe/03_аccount.py
@@ -0,0 +1,25 @@
+class Account:
+    def __init__(self, _id: int, name: str, balance: float = 0) -> None:
+        self._id = _id
+        self.name = name
+        self.balance = balance
+
+    def credit(self, amount: float) -> float:
+        self.balance += amount
+        return self.balance
+
+    def debit(self, amount: float) -> float | str:
+        if self.balance < amount:
+            return 'Amount exceeded balance'
+
+        self.balance -= amount
+        return self.balance
+
+    def info(self) -> str:
+        return f'User {self.name} with account {self._id} has {self.balance} balance'
+
+
+account = Account(1234, "George", 1000)
+print(account.credit(500))
+print(account.debit(1501))
+print(account.info())
