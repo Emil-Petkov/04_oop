bab5505d38a5e0cb68c8d0f25b299378a7f7ea56
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-07-21 14:37:11 +0300

    .

---
+++ 03_inheritance/01_lab/04_multilevel_inheritance/project/main.py
@@ -0,0 +1,12 @@
+from project.car import Car
+from project.sports_car import SportsCar
+
+c = Car()
+sc = SportsCar()
+
+print(c.move())
+print(c.drive())
+print()
+print(sc.move())
+print(sc.drive())
+print(sc.race())
