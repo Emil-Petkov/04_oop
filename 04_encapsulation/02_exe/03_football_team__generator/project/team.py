49868becaabb817d8958d44bd0a0b671bfcd9143
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-07-24 14:41:53 +0300

    .

--- 04_encapsulation/02_exe/03_football_team__generator/project/team.py
+++ 04_encapsulation/02_exe/03_football_team__generator/project/team.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 
 from project.player import Player
 
@@ -16,13 +16,13 @@ class Team:
         self.__players.append(player)
         return f'Player {player.name} joined team {self.__name}'
 
-    def remove_player(self, player_name: str) -> str:
-        for p in self.__players:
-            if p.name == player_name:
-                self.__players.remove(p)
-                return p
+    def remove_player(self, player_name: str) -> Union[Player, str]:
+        try:
+            player = [p for p in self.__players if p.name == player_name][0]
+            return player
 
-        return f'Player {player_name} not found'
+        except IndexError:
+            return f'Player {player_name} not found'
 
     @property
     def players(self):
