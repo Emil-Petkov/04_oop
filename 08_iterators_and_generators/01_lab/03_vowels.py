76334010326b1e90857e844b0e5eb23020120739
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2024-08-08 19:26:27 +0300

    Update 03_vowels.py

--- 08_iterators_and_generators/01_lab/03_vowels.py
+++ 08_iterators_and_generators/01_lab/03_vowels.py
@@ -6,14 +6,14 @@ class vowels:
         self.vowels_values = [c for c in self.string if c.lower() in self.vowels_list]
 
     def __iter__(self):
-        return self
+        return iter(self.vowels_values)  # self
 
-    def __next__(self):
-        self.index += 1
-        if self.index < len(self.vowels_values):
-            return self.vowels_values[self.index]
-
-        raise StopIteration
+        # def __next__(self):
+        #     self.index += 1
+        #     if self.index < len(self.vowels_values):
+        #         return self.vowels_values[self.index]
+        #
+        #     raise StopIteration
 
 
 my_string = vowels('Abcedifuty0o')
